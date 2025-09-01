f06a1e0f1e52e57d33c002648713ff556bec88c0
Mauricio Stuart <mainjettdovava@hotmail.com>
2025-09-01 12:08:45 -0300

    Hub de atividades - Cursos Alura sobre C#

---
+++ Program.cs
@@ -0,0 +1,228 @@
+﻿void MenuGeral()
+
+{
+    string linkedin = $"https://www.linkedin.com/in/mauricio-stuart-fontes-83b45318a";
+    string gitHub = $"https://github.com/stuartfontes";
+Console.WriteLine(@"
+█░█ █░█ █▄▄   █▀▄ █▀▀   ▄▀█ ▀█▀ █ █░█ █ █▀▄ ▄▀█ █▀▄ █▀▀ █▀
+█▀█ █▄█ █▄█   █▄▀ ██▄   █▀█ ░█░ █ ▀▄▀ █ █▄▀ █▀█ █▄▀ ██▄ ▄█");
+
+    Console.WriteLine("\nEsse é o menu geral de atividades da Alura\n" +
+                      "Foram realizadas por mim e transformadas em um Hub para melhor visualição\n" +
+                      "Acesse as redes;\n" +
+                      $"Linkedin: {linkedin}\n" +
+                      $"GitHub: {gitHub}\n");
+}
+
+void hub()
+{
+    Console.WriteLine("Digito 1: Acessar a atividade 'Exibindo n-ésima posição em lista'\n");
+    Console.WriteLine("Digito 2: Acessar a atividade 'Calculador de números em Lista'\n");
+
+    switch (Console.ReadKey(intercept: true).Key)
+    {
+        case ConsoleKey.D1: Console.WriteLine("\nVocê escolheu a Atividade 1;" +
+                                              "\nPara mais informações pressione ENTER\n");
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.Enter:
+                    Console.WriteLine("Essa atividade consiste em uma lista de números escolhidos pelo usuário" +
+                                      "que irá ser visualizada pós finalização e, então, escolhido uma posição" +
+                                      "de escolha do usuário para ser visualizada\n");
+                    Console.WriteLine("Pressione Enter para prosseguir para Atividade n°1;" +
+                                      "\nPressione BACKSPACE para retornar ao Hub;");
+                    switch (Console.ReadKey(intercept:true).Key)
+                    {
+                        case ConsoleKey.Enter:
+                            Console.WriteLine("Prosseguindo para Atividade n°1;\nAguarde...");
+                            Thread.Sleep(2000);
+                            Console.Clear();
+                            menuAtividade1();
+                            List<int> listaDeNumeros = listas();
+                            posicoes(listaDeNumeros);
+                            break;
+                        case ConsoleKey.Backspace:
+                            Console.WriteLine("Retornando ao Hub;\nAguarde...");
+                            Thread.Sleep(2000);
+                            Console.Clear();
+                            MenuGeral();
+                            hub();
+                            break;
+                        default:
+                            Console.WriteLine("\nErro: Caracter inválido;\nPressione ENTER ou BACKSPACE;" +
+                                              "\nRetornando ao Hub;\nAguarde...");
+                            MenuGeral();
+                            hub();
+                            break;
+                            
+                    }
+                    Thread.Sleep(2000);
+                    Console.Clear();
+              
+                    break;
+               default:
+                   Console.WriteLine("\nErro: Caracter inválido;\nPressione ENTER ou BACKSPACE;" +
+                                     "\nRetornando ao Hub;\nAguarde...");
+                   hub();
+                   break;
+            }  
+            break;
+            
+        case ConsoleKey.D2: Console.WriteLine("\nVocê escolheu a Atividade 2;" +
+                                              "\nPara mais informações pressione ENTER");
+            switch (Console.ReadKey(intercept:true).Key)
+            {
+                case ConsoleKey.Enter:
+                    Console.WriteLine("Essa atividade consiste na listagem, soma, subtração, multiplicação e divisão" +
+                                      "de todos os números listados pelo usuário;\n");
+                    Console.WriteLine("Pressione ENTER para prosseguir para Atividade 2;\n" +
+                                      "Pressione BACKSPACE para retornar ao Hub;");
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.Enter:
+                            Console.WriteLine("Prosseguindo para Atividade 2;\nAguarde...");
+                            Thread.Sleep(2000);
+                            Console.Clear();
+                            menuAtividade2();
+                            break;
+                        case ConsoleKey.Backspace:
+                            Console.WriteLine("Retornando ao Hub;\nAguarde...");
+                            Thread.Sleep(2000);
+                            Console.Clear();
+                            hub();
+                            break;
+                    }
+                    break;
+            }
+            
+            Thread.Sleep(2000);
+            Console.Clear();
+            break;
+        
+        default:
+            Console.WriteLine("\nErro: Caracter inválido;\nPressione ENTER ou BACKSPACE;" +
+                              "\nRetornando ao Hub;\nAguarde...");
+            Thread.Sleep(2000);
+            Console.Clear();
+            MenuGeral();
+            hub();
+            break;
+    }
+}
+
+
+MenuGeral();
+hub();
+
+// Atividade 1 - Escrever  um programa que dado a entrada de dados de um número N inteiro, a partir do teclado
+// exibir a n-ésima posição de uma lista (baseado na escolha do usuário)
+void menuAtividade1()
+{
+    Console.WriteLine("Seja bem vindo a Atividade n°1, será breve e irá entregar uma lista," +
+                      " insira seus digitos e posteriormente escolha qual será o número que desja informar na tela\n");
+}
+
+menuAtividade1();
+
+
+List<int> listas()
+{
+    Console.Write("Insira sua lista de números, lembre de separa-las por um espaço: ");
+    string entrada = Console.ReadLine();
+    string entradaSemEspaço = entrada.Trim();
+        
+    List<int> numerosAtv1 = entradaSemEspaço
+        .Split(" ")
+        .Select(int.Parse)
+        .ToList();
+        
+
+    Console.WriteLine("Você digitou os seguintes números: "+string.Join(" ",numerosAtv1)); 
+    
+    
+    return numerosAtv1;
+    
+}
+
+void posicoes(List<int> numerosAtv1)
+
+{
+    Console.Write("Insira a posição que deseja visualizar na lista: ");
+    int n = int.Parse(Console.ReadLine());
+
+    if (n > 0 && n <= numerosAtv1.Count)
+    {
+        Console.WriteLine($"O número na posição {n} é: {numerosAtv1[n - 1]}");
+        Console.WriteLine("Pressione ENTER para gerar outra lista\nPressione BACKSPACE para retornar ao Hub;");
+        switch (Console.ReadKey(intercept: true).Key)
+        {
+            case ConsoleKey.Enter:
+                Console.WriteLine("Gerando novas listas;\nAguarde...");
+                Thread.Sleep(2000);
+                Console.Clear();
+                menuAtividade1();
+                List<int> listaDeNumeros = listas();
+                posicoes(listaDeNumeros);
+                break;
+            case ConsoleKey.Backspace:
+                Console.WriteLine("Retornando ao Hub;\nAguarde...");
+                Thread.Sleep(2000);
+                Console.Clear();
+                MenuGeral();
+                hub();
+                break;
+            default:
+                Console.WriteLine("\nErro: Caractere inválido;\nPressione ENTER ou BACKSPACE;" +
+                                  "\nRetornando a Atividade n°1;\nAguarde...");
+                Thread.Sleep(2000);
+                Console.Clear();
+                menuAtividade1();
+                break;
+        }
+        
+    }
+    else
+    {
+        Console.WriteLine("Posição inválida\nDigite uma posição válida baseada na quantidade de números digitados;\n");
+        posicoes(numerosAtv1);
+    }    
+    
+    
+}
+
+List<int> listaDeNumeros = listas();
+posicoes(listaDeNumeros);
+
+// Atividade 2 - Criar um programa que calcula as quatro operações de todos os elementos inteiros de uma lista, 
+// utilizando a interpolação de Strings 
+
+void menuAtividade2()
+{
+    
+    Console.WriteLine("Seja bem vindo a Atividade n°2;" +
+                      "\nInsira os números que deseja somar, subtrair, multiplicar e dividir");
+}
+
+List<int> listas2()
+{
+     Console.Write("Insira os números que deseja somar/subtrair/multiplicar e dividir: ");
+     string numerosAtv2 = Console.ReadLine();
+    List<int> dados = numerosAtv2
+        .Split(" ")
+        .Select(int.Parse)
+        .ToList();
+    Console.WriteLine("Você digitou os seguintes números: "+string.Join(" ",dados)); 
+
+    return dados;
+}
+
+void somas(List<int> numerosAtv2)
+{
+    Console.WriteLine("Realizando as quatro operações matemáticas dos números introduzidos;\nAguarde...\n");
+    
+
+}
+List<int> listaDeNumerosSomar = listas2();
+somas(listaDeNumerosSomar);
+menuAtividade2();
+
